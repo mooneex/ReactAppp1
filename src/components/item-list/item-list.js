4bde00fb3367751ec3ad5bd75831c76d19ddc233
mooneex <123557489+mooneex@users.noreply.github.com>
2023-12-04 15:18:16 +0600

    Add Row component

--- src/components/item-list/item-list.js
+++ src/components/item-list/item-list.js
@@ -20,12 +20,9 @@ export default class ItemList extends Component {
             })
     }
 
-    renderItems = (arr) => {
+    renderItems(arr) {
         return arr.map((item) => {
             const {id} = item;
-
-            if(!this.props.renderItem) debugger;
-
             const label = this.props.renderItem(item);
             return (
                 <li
@@ -36,7 +33,7 @@ export default class ItemList extends Component {
                     {label}
                 </li>)
         });
-    };
+    }
 
 
     render() {
