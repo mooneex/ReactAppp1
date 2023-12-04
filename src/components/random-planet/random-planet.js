508a0ec5a39aeecf31a78a5c57797316d8c3f0fd
mooneex <123557489+mooneex@users.noreply.github.com>
2023-12-04 15:10:52 +0600

    Code refactoring

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
-import SwapiService from "../../services/swapi-service";
-import Spinner from '../spinner'
+import SwapiService from '../../services/swapi-service';
+import Spinner from '../spinner';
 import ErrorIndicator from "../error-indicator";
 import './random-planet.css';
 
@@ -14,24 +14,24 @@ export default class RandomPlanet extends Component {
         error: false
     };
 
-    constructor() {
-        super();
+    componentDidMount() {
         this.updatePlanet();
+        this.interval = setInterval( this.updatePlanet, 60000 )
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.interval)
     }
 
     onPlanetLoaded = (planet) => {
         this.setState({
             planet,
-            loading: false
-        })
+            loading: false,
+            error: false
+        });
     };
 
-    updatePlanet() {
-        const id = Math.floor(Math.random() * 25 + 2);
-        this.swapiService.getPlanet(id)
-            .then(this.onPlanetLoaded)
-            .catch(this.onError)
-    }
+    
     onError = (err) => {
         this.setState({
             error: true,
@@ -39,8 +39,18 @@ export default class RandomPlanet extends Component {
         })
     };
 
+    updatePlanet = () => {
+        const id = Math.floor( Math.random() * 17 ) + 3;
+
+        this.swapiService
+            .getPlanet(id)
+            .then(this.onPlanetLoaded)
+            .catch(this.onError);
+    };
+
+
     render() {
-        const { planet, loading, error} = this.state;
+        const {planet, loading, error} = this.state;
 
         const hasData = !(loading || error);
         const errorMessage = error ? <ErrorIndicator /> : null;
@@ -62,7 +72,7 @@ const PlanetView = ({planet}) => {
     return (
         <React.Fragment>
             <img className="planet-image"
-                src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`} alt="planet"/>
+                src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`} />
             <div>
                 <h4>{name}</h4>
                 <ul className="list-group list-group-flush">
@@ -81,5 +91,5 @@ const PlanetView = ({planet}) => {
                 </ul>
             </div>
         </React.Fragment>
-    );
+    )
 };
--- src/components/spinner/spinner.js
+++ src/components/spinner/spinner.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './spinner.css';
 
-export default () => {
+const Spinner = () => {
     return (
         <div className="lds-css">
             <div className="lds-double-ring">
@@ -11,6 +11,8 @@ export default () => {
         </div>
     );
 };
+
+export default Spinner;
 
 
 
